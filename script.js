5d57c6c63814a84acf26374f8694d173c707f3ed
diegotyner <120358477+diegotyner@users.noreply.github.com>
2024-06-02 14:47:48 -0700

    Filtering almost working

--- script.js
+++ script.js
@@ -48,12 +48,13 @@ window.onload = async function() {
   console.log(today.toISOString().split('T')[0]);
   console.log(today.toUTCString());
 
-  createCalendarCardList(testing_calendarList);
-  // for (const user of payload[1]) { // Assuming that this is the calendars
-  //   for (const cal of user.calendarList) { // Going through each user to get calendars
-  //     shownCalendars[cal.id] = cal
-  //   }
-  // }
+  createCalendarCardList(payload[1]);
+  for (const user of payload[1]) { // Assuming that this is the calendars
+    for (const cal of user.calendarList) { // Going through each user to get calendars
+      shownCalendars.add(cal.id)
+    }
+  }
+  console.log(shownCalendars)
 
   for (let i =0; i<7; i++) {
     const curDay = new Date();
@@ -62,7 +63,7 @@ window.onload = async function() {
 
     
     console.log(UTC_local);
-    const list_dayEvents = payload[UTC_local];
+    const list_dayEvents = payload[0][UTC_local];
     updateCalendar(list_dayEvents, i, UTC_local);
   }    
   initialized = true;
@@ -73,6 +74,25 @@ document.getElementById("logInButton").onclick = function () {
   window.open("https://calendar-back-end-snowy.vercel.app/auth/google/");
 };
 
+function updateShownCalendars() {
+  var counter = 0;
+  console.log("------------------------")
+  console.log(eventDatabase.entries)
+  for (let [formatDate, dayEvents] of eventDatabase.entries()) {
+    const newDayEvents = []
+    for (const e of dayEvents) {
+      if (e.calendarID in shownCalendars) {
+        newDayEvents.push(e);
+        console.log("i shown")
+      } else {
+        console.log("i am not shown")
+        console.log(e.calendarID)
+      }
+    }
+    updateCalendar(newDayEvents, counter, formatDate);
+    counter += 1;
+  }
+}
 
 /* Assuming the cal list will have the following structure:
       list -> users -> cals
@@ -90,7 +110,7 @@ function createCalendarCardList(calendarList) {
     toggler.classList.add("navbar-toggler")
     toggler.setAttribute("type", "button");
     toggler.setAttribute("data-toggle", "collapse");
-    toggler.setAttribute("data-target", "#" + user.username + "_collapse");
+    toggler.setAttribute("data-target", "#" + user.username.split(" ")[0] + "_collapse");
     
     let span = document.createElement("span");
     span.classList.add("navbar-toggler-icon");
@@ -98,15 +118,18 @@ function createCalendarCardList(calendarList) {
     usernameRow.appendChild(toggler);
 
     let username = document.createElement("span");
-    username.innerHTML = user.username;  // WILL NEED TO UPDATE
+    username.innerHTML = user.username.split(" ")[0];
     usernameRow.appendChild(username);
 
 
     let userCard = document.createElement("div");
     userCard.classList.add("userCard", "collapse", "navbar-expand-xxl");
-    userCard.setAttribute("id", user.username + "_collapse");
+    userCard.setAttribute("id", user.username.split(" ")[0] + "_collapse");
 
     for (const cal of user.calendarList) {
+      if (cal.id.endsWith("group.v.calendar.google.com")) { // If its this calendar, its an irrelevant Google Calendar (holidays, etc)
+        continue
+      }
       let calendarCard = document.createElement("div");
       calendarCard.classList.add("calendarCard");
       userCard.appendChild(calendarCard);
@@ -119,11 +142,13 @@ function createCalendarCardList(calendarList) {
         if (this.checked) {
           shownCalendars.add(cal.id)
           updateShownCalendars()
-          console.log("on", cal.id)
+          console.log("------- done -------")
         } else {
           shownCalendars.delete(cal.id);
           updateShownCalendars()
-          console.log("off", cal.id)
+          console.log(cal.id);
+          console.log(shownCalendars);
+          console.log("------- done -------")
         }
       })
       calendarCard.appendChild(checkbox);
@@ -137,19 +162,7 @@ function createCalendarCardList(calendarList) {
   }
 }
 
-function updateShownCalendars() {
-  var counter = 0;
-  for (const [formatDate, dayEvents] of eventDatabase.entries()) {
-    const newDayEvents = []
-    for (const e of dayEvents) {
-      if (e.calID in shownCalendars) {
-        newDayEvents.push(e);
-      }
-    }
-    updateCalendar(newDayEvents, counter, formatDate);
-    counter += 1;
-  }
-}
+
 
 
 function formatLocaleDate(locale_date) {
@@ -161,7 +174,7 @@ function formatLocaleDate(locale_date) {
 
 async function getData() {
   try {
-    const response = await axios.get('https://calendar-back-end-snowy.vercel.app/home');
+    const response = await axios.get('http://localhost:3000/home');
     return response.data; // Return the data to be used in the calling function
   } catch (error) {
       console.error('Error fetching data:', error);
@@ -215,185 +228,185 @@ function createCalendar() {
 
 
 function updateCalendar(event_list, day, formatted_date) {
-    var STARTTIME = 0,
-      ENDTIME = 24,
-      HEIGHTOFHOUR = 40,
-      h, m, e,
-      ts, event, leftindex;
-    
-    var MINUTESINDAY = (ENDTIME - STARTTIME) * 60;
-    
-    var timeslots = [];
-    for (m=0; m<MINUTESINDAY; m++) {
-      timeslots.push([]);
-    }
-    
-    // the eventids will probably come from a database and cannot be numeric so we
-    // use the EventsById object as a kind of lookup - well use the ids as properties of this
-    // object and then get them using array notation.
-    var EventsById = {};
-    const events = setUpEvents(event_list);
-    if (!initialized) {
-      eventDatabase[formatted_date] = events;
-    }
+  var STARTTIME = 0,
+    ENDTIME = 24,
+    HEIGHTOFHOUR = 40,
+    h, m, e,
+    ts, event, leftindex;
+  
+  var MINUTESINDAY = (ENDTIME - STARTTIME) * 60;
+  
+  var timeslots = [];
+  for (m=0; m<MINUTESINDAY; m++) {
+    timeslots.push([]);
+  }
+  
+  // the eventids will probably come from a database and cannot be numeric so we
+  // use the EventsById object as a kind of lookup - well use the ids as properties of this
+  // object and then get them using array notation.
+  var EventsById = {};
+  const events = setUpEvents(event_list);
+  if (!initialized) {
+    eventDatabase.set(formatted_date, events);
+  }
 
-    // load events into timeslots - events must be sorted by starttime already
+  // load events into timeslots - events must be sorted by starttime already
+  var numEvents = events.length;
+  for (e=0; e<numEvents; e++) {
+    event = events[e];
+    for (m=event.start; m<event.stop; m++) {
+      timeslots[m].push(event.id);
+    }
+  }
+  
+  // take the timeslots one at a time
+  // for each event in the timeslot make sure that it has the right numcolumns (max amount for that event)
+  // then check if its leftindex has been set
+  // if not then set it.  find the first free space in that timeslot
+  for (m=0; m<MINUTESINDAY; m++) {
+    ts = timeslots[m];
+    for (e=0; e<ts.length; e++) {
+      event = EventsById[ ts[e] ];
+      var max = ts.length;
+      ts.forEach(function(id){
+          var evt = EventsById[id];
+          max=(evt.numcolumns>max)?evt.numcolumns:max;
+        });
+    
+      if (event.numcolumns <= max) {    
+        event.numcolumns = max;
+      }
+      
+      if (event.leftindex == -1) {
+        leftindex = 0;
+        while (! isFreeSpace(ts, leftindex, event.id)) {
+            leftindex++;
+        }
+        event.leftindex = leftindex;
+      }
+    }
+  }
+  // UPDATE CODE AFTER COMMENT
+  // fix numcolumns
+  for (m=0; m<MINUTESINDAY; m++) {
+    ts = timeslots[m];
+    for (e=0; e<ts.length; e++) {
+      event = EventsById[ ts[e] ];
+      var max = ts.length;
+      ts.forEach(function(id){
+          var evt = EventsById[id];
+          max=(evt.numcolumns>max)?evt.numcolumns:max;
+        });
+    
+      if (event.numcolumns <= max) {    
+        event.numcolumns = max;
+      }
+    }
+  }
+  
+  layoutEvents(day);
+  
+  function isFreeSpace(ts, leftindex, eventid) {
+    var tslength = ts.length;
+    var event;
+    for (var i=0; i<tslength; ++i) {
+      // get the event in this timeslot location
+      event = EventsById[ts[i]];
+      if (event.leftindex == leftindex) {
+        if (event.id != eventid) {
+          return false; // left index taken
+        } else {
+          return true; // this event is in this place
+        }
+      }
+    }
+    return true;
+  }
+  
+  function setUpEvents(event_list) {
+      const events = event_list.filter(event => event.starttime.split('T').length > 1);
+      events.forEach(event => {
+        event.starttime = event.starttime.split('T')[1]
+        event.endtime = event.endtime.split('T')[1]
+      })
+
+      events.sort(function(a, b) {
+        var posa = a.starttime.indexOf(':');
+        var posb = b.starttime.indexOf(':');
+      
+        var hours_a = parseInt(a.starttime.substr(0, posa), 10);
+        var minutes_a = parseInt(a.starttime.substr(posa + 1, 2), 10);
+        var hours_b = parseInt(b.starttime.substr(0, posb), 10);
+        var minutes_b = parseInt(b.starttime.substr(posb + 1, 2), 10);
+
+        var size_time_a = hours_a + minutes_a / 60;
+        var size_time_b = hours_b + minutes_b / 60;
+        return size_time_a - size_time_b;
+      });
+
+      var numEvents = events.length;
+      var event, e, pos, stH, stM, etH, etM, height;
+
+      for (e=0; e<numEvents; e++) {
+          event = events[e];
+          event.leftindex = -1;
+          event.numcolumns = 0;
+          pos = event.starttime.indexOf(':');
+          stH = parseInt( event.starttime.substr(0, pos), 10);
+          stM = parseInt( event.starttime.substr(pos+1), 10) / 60;
+          // need its positions top and bottom in minutes
+          event.start = ((stH - STARTTIME) * 60) + (stM * 60);
+          event.topPos = ((stH - STARTTIME) * HEIGHTOFHOUR) + (stM * HEIGHTOFHOUR);
+          
+          pos = event.endtime.indexOf(':');
+          etH = parseInt( event.endtime.substr(0, pos), 10);
+          etM = parseInt( event.endtime.substr(pos+1), 10) / 60;
+          // need its positions top and bottom in minutes
+          event.stop = ((etH - STARTTIME) * 60) + (etM * 60);
+          
+          height = (etH - stH) * HEIGHTOFHOUR;
+          height -= stM * HEIGHTOFHOUR;
+          height += etM * HEIGHTOFHOUR;
+          event.height = height;
+          EventsById[event.id] = event;
+      }  
+      return events
+  }
+  
+  function layoutEvents(column_id) {
     var numEvents = events.length;
+    var event, e, numx, xfactor, left;
+    
     for (e=0; e<numEvents; e++) {
       event = events[e];
-      for (m=event.start; m<event.stop; m++) {
-        timeslots[m].push(event.id);
-      }
-    }
-    
-    // take the timeslots one at a time
-    // for each event in the timeslot make sure that it has the right numcolumns (max amount for that event)
-    // then check if its leftindex has been set
-    // if not then set it.  find the first free space in that timeslot
-    for (m=0; m<MINUTESINDAY; m++) {
-      ts = timeslots[m];
-      for (e=0; e<ts.length; e++) {
-        event = EventsById[ ts[e] ];
-        var max = ts.length;
-        ts.forEach(function(id){
-            var evt = EventsById[id];
-            max=(evt.numcolumns>max)?evt.numcolumns:max;
-          });
       
-        if (event.numcolumns <= max) {    
-          event.numcolumns = max;
-        }
-       
-        if (event.leftindex == -1) {
-          leftindex = 0;
-          while (! isFreeSpace(ts, leftindex, event.id)) {
-              leftindex++;
-          }
-          event.leftindex = leftindex;
-        }
-      }
-    }
-    // UPDATE CODE AFTER COMMENT
-    // fix numcolumns
-    for (m=0; m<MINUTESINDAY; m++) {
-      ts = timeslots[m];
-      for (e=0; e<ts.length; e++) {
-        event = EventsById[ ts[e] ];
-        var max = ts.length;
-        ts.forEach(function(id){
-            var evt = EventsById[id];
-            max=(evt.numcolumns>max)?evt.numcolumns:max;
-          });
+      numx = event.numcolumns;
+      xfactor = 1 / numx;
+      left = (event.leftindex * xfactor * 100);
       
-        if (event.numcolumns <= max) {    
-          event.numcolumns = max;
-        }
-      }
-    }
-    
-    layoutEvents(day);
-    
-    function isFreeSpace(ts, leftindex, eventid) {
-      var tslength = ts.length;
-      var event;
-      for (var i=0; i<tslength; ++i) {
-        // get the event in this timeslot location
-        event = EventsById[ts[i]];
-        if (event.leftindex == leftindex) {
-          if (event.id != eventid) {
-            return false; // left index taken
-          } else {
-            return true; // this event is in this place
-          }
-        }
-      }
-      return true;
-    }
-    
-    function setUpEvents(event_list) {
-        const events = event_list.filter(event => event.starttime.split('T').length > 1);
-        events.forEach(event => {
-          event.starttime = event.starttime.split('T')[1]
-          event.endtime = event.endtime.split('T')[1]
-        })
 
-        events.sort(function(a, b) {
-          var posa = a.starttime.indexOf(':');
-          var posb = b.starttime.indexOf(':');
-        
-          var hours_a = parseInt(a.starttime.substr(0, posa), 10);
-          var minutes_a = parseInt(a.starttime.substr(posa + 1, 2), 10);
-          var hours_b = parseInt(b.starttime.substr(0, posb), 10);
-          var minutes_b = parseInt(b.starttime.substr(posb + 1, 2), 10);
-
-          var size_time_a = hours_a + minutes_a / 60;
-          var size_time_b = hours_b + minutes_b / 60;
-          return size_time_a - size_time_b;
-        });
-
-        var numEvents = events.length;
-        var event, e, pos, stH, stM, etH, etM, height;
-
-        for (e=0; e<numEvents; e++) {
-            event = events[e];
-            event.leftindex = -1;
-            event.numcolumns = 0;
-            pos = event.starttime.indexOf(':');
-            stH = parseInt( event.starttime.substr(0, pos), 10);
-            stM = parseInt( event.starttime.substr(pos+1), 10) / 60;
-            // need its positions top and bottom in minutes
-            event.start = ((stH - STARTTIME) * 60) + (stM * 60);
-            event.topPos = ((stH - STARTTIME) * HEIGHTOFHOUR) + (stM * HEIGHTOFHOUR);
-            
-            pos = event.endtime.indexOf(':');
-            etH = parseInt( event.endtime.substr(0, pos), 10);
-            etM = parseInt( event.endtime.substr(pos+1), 10) / 60;
-            // need its positions top and bottom in minutes
-            event.stop = ((etH - STARTTIME) * 60) + (etM * 60);
-            
-            height = (etH - stH) * HEIGHTOFHOUR;
-            height -= stM * HEIGHTOFHOUR;
-            height += etM * HEIGHTOFHOUR;
-            event.height = height;
-            EventsById[event.id] = event;
-        }  
-        return events
-    }
-    
-    function layoutEvents(column_id) {
-      var numEvents = events.length;
-      var event, e, numx, xfactor, left;
       
-      for (e=0; e<numEvents; e++) {
-        event = events[e];
-        
-        numx = event.numcolumns;
-        xfactor = 1 / numx;
-        left = (event.leftindex * xfactor * 100);
-        
+      // Create a new div element
+      var cal_data = document.createElement("div");
+      cal_data.className = "cal-data";
+      cal_data.id = "cal-data-" + event.id;
+      cal_data.innerHTML = "<h4>" + event.starttime + " - " + event.endtime + '#' + numx + "</h4>";
+      cal_data.style.top = Math.round(event.topPos) + "px";
+      cal_data.style.height = Math.round(event.height) + "px";
+      cal_data.style.width = Math.floor(100 * xfactor) + "%";
+      cal_data.style.left = left + "%";
 
-        
-        // Create a new div element
-        var cal_data = document.createElement("div");
-        cal_data.className = "cal-data";
-        cal_data.id = "cal-data-" + event.id;
-        cal_data.innerHTML = "<h4>" + event.starttime + " - " + event.endtime + '#' + numx + "</h4>";
-        cal_data.style.top = Math.round(event.topPos) + "px";
-        cal_data.style.height = Math.round(event.height) + "px";
-        cal_data.style.width = Math.floor(100 * xfactor) + "%";
-        cal_data.style.left = left + "%";
-
-        // console.log(event)
-        if (event.user == "diegotyner59000@gmail.com") {
-          cal_data.classList.add("user1")
-        } else {
-          cal_data.classList.add("user2")
-        }
-
-        document.getElementById(column_id).appendChild(cal_data);
-
+      // console.log(event)
+      if (event.user == "diegotyner59000@gmail.com") {
+        cal_data.classList.add("user1")
+      } else {
+        cal_data.classList.add("user2")
       }
+
+      document.getElementById(column_id).appendChild(cal_data);
+
     }
+  }
 };
 
 
